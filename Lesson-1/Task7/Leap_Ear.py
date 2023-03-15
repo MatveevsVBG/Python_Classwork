2091de144831d20861554083cb25e0a5e1873508
Sergei Matveev <msergne@gmail.com>
2023-03-15 13:53:08 +0300

    Change Task7

--- Lesson-1/Task7/Leap_Ear.py
+++ Lesson-1/Task7/Leap_Ear.py
@@ -8,8 +8,17 @@
 високосным, если его номер кратен 4, но не кратен
 100, а также если он кратен 400.
 """
-year = int(input("Введите год: "))
-if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    print('Yes')
+
+class MyException(Exception):
+    pass
+try:
+    year = int(input("Введите год: "))
+    if year < 1000 or year > 9999:
+        raise MyException()
+except MyException:
+    print("Неверный формат ввода!")
 else:
-    print('No')
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        print('Yes')
+    else:
+        print('No')
